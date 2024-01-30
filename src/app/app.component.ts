2be2729b350c90cfdc76c02c5c8dae2fcf31f71b
MichelHdez <michel01406@gmail.com>
2024-01-29 18:16:37 -0600

    Project Dados Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,18 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Piano';
+  dadoIzquierda = '../assets/img/dice1.png';
+  dadoDerecha = '../assets/img/dice4.png';
+  numero1 = 1;
+  numero2 = 2;
+
+  tirarDados(): void {
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;
+    this.dadoIzquierda = '../assets/img/dice' + this.numero1 + '.png';
+    this.dadoDerecha = '../assets/img/dice' + this.numero2 + '.png';
+    console.log(this.numero1);
+    console.log(this.numero2);
+
+  }
 }
